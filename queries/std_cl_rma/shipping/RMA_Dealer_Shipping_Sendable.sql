e04389a94091af12a19aa163a7606b1a9ae13106
Bernt Gudmestad <bernt@xspeed.co>
2017-09-08 15:01:18 -0700

    rearranged files, added std/cl/rma queries that will be changed as well

---
+++ queries/std_cl_rma/shipping/RMA_Dealer_Shipping_Sendable.sql
@@ -0,0 +1,11 @@
+SELECT s.order_id, s.customer_id, s.shipping_id, s.sales_ofc, s.sales_grp, s.doc_type,
+s.Email_OTC AS 'Email Address', s.Email_OTC AS SubscriberKey
+
+FROM FromSAP_Shipping s
+
+WHERE
+(s.sales_ofc = 'STD' OR s.sales_ofc = 'HYB')
+AND s.doc_type = 'ZARF'
+AND s.consumed = 'false'
+AND DATEDIFF(dd, s.[Date Added], GETDATE()) < 15
+AND s.Email_OTC IS NOT NULL
